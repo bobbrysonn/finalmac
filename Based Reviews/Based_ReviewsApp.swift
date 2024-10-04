9202a64877337d4e92287493d82aa094cf6641bb
Bob Moriasi <official.bobmoriasi@gmail.com>
2024-10-03 22:18:33 -0400

    Initial Commit

---
+++ Based Reviews/Based_ReviewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Based_ReviewsApp.swift
+//  Based Reviews
+//
+//  Created by Bob Moriasi on 10/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Based_ReviewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
